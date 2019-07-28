151ea52f8fbd9e1bcccb4137c301e28f97f8b56a
Zalo <cebolleto@gmail.com>
2019-07-28 14:39:03 +0200

    pollo destroy anim

--- src/SpritePollo.c
+++ src/SpritePollo.c
@@ -3,6 +3,8 @@
 #include "SpriteManager.h"
 #include "Scroll.h"
 
+UINT8 anim_pollo_dying[] = {6, 1, 2, 3, 4, 5, 5};
+
 struct PolloInfo {
 	INT8 vy;
 };
@@ -22,10 +24,17 @@ void Update_SpritePollo() {
 	struct PolloInfo* data = (struct PolloInfo*)THIS->custom_data;
 	UINT8 tile_coll;
 
-	if(data->vy) {
-		if(tile_coll = TranslateSprite(THIS, 0, data->vy)) {
-			if(tile_coll >= 15 && tile_coll <= 24)
-				SpriteManagerRemove(THIS_IDX);
+	if(THIS->anim_data) {
+		if(THIS->current_frame == 5) {
+			SpriteManagerRemove(THIS_IDX);
+		}
+	} else {
+		if(data->vy) {
+			if(tile_coll = TranslateSprite(THIS, 0, data->vy)) {
+				if(tile_coll >= 15 && tile_coll <= 24) {
+					SetSpriteAnim(THIS, anim_pollo_dying, 15);
+				}
+			}
 		}
 	}
 }
