8787583e2704ecfba93e8dde333981bcd37f5da5
Zalo <cebolleto@gmail.com>
2019-07-28 15:56:47 +0200

    improved chicken

--- src/SpriteMouth.c
+++ src/SpriteMouth.c
@@ -1,13 +1,28 @@
 #include "Banks/SetBank2.h"
 
 #include "SpriteManager.h"
+#include "ZGBMain.h"
 UINT8 mouth_anim[] = {10, 0, 0, 0, 0, 1, 2, 2, 2, 2, 1};
+UINT8 mouth_anim_munch[] = {4, 0, 1, 2, 1};
 
 void Start_SpriteMouth() {
 	SetSpriteAnim(THIS, mouth_anim, 13);
 }
 
 void Update_SpriteMouth() {
+	UINT8 i;
+	struct Sprite* spr;
+
+	SPRITEMANAGER_ITERATE(i, spr) {
+		if(spr->type == SpritePollo) {
+			if(!spr->anim_data && CheckCollision(THIS, spr)) {
+				if(THIS->anim_data[THIS->current_frame] == 0) {
+					SpriteManagerRemove(i);
+					SetSpriteAnim(THIS, mouth_anim_munch, 32);
+				}
+			}
+ 		}
+	}
 }
 
 void Destroy_SpriteMouth() {
--- src/StateGame.c
+++ src/StateGame.c
@@ -10,7 +10,7 @@
 #include "Print.h"
 #include "../res/src/font.h"
 
-UINT8 collision_tiles[] = {1, 2, 6, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 22, 23, 24, 27, 28, 29, 30, 0};
+UINT8 collision_tiles[] = {1, 2, 3, 6, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 39, 0};
 extern struct Sprite* sprite_chopter;
 
 void Start_StateGame() {
