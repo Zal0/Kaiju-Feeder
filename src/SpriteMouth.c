aa9eddc005ad72e1dce0fca97a937dd45e32ce62
Zalo <cebolleto@gmail.com>
2019-07-28 13:40:21 +0200

    Mouth Anim

--- src/SpriteMouth.c
+++ src/SpriteMouth.c
@@ -1,10 +1,10 @@
 #include "Banks/SetBank2.h"
 
 #include "SpriteManager.h"
-UINT8 mouth_anim[] = {2, 0, 1};
+UINT8 mouth_anim[] = {10, 0, 0, 0, 0, 1, 2, 2, 2, 2, 1};
 
 void Start_SpriteMouth() {
-	SetSpriteAnim(THIS, mouth_anim, 1);
+	SetSpriteAnim(THIS, mouth_anim, 13);
 }
 
 void Update_SpriteMouth() {
