645b581b0f674eb62d2ba26372287551a1144c2c
Zalo <cebolleto@gmail.com>
2019-07-28 17:52:01 +0200

    adjusted coll tiles

--- src/StateGame.c
+++ src/StateGame.c
@@ -13,7 +13,7 @@
 #include "Print.h"
 #include "../res/src/font.h"
 
-UINT8 collision_tiles[] = {1, 2, 3, 6, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 39, 0};
+UINT8 collision_tiles[] = {1, 2, 3, 6, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 33, 39, 0};
 extern struct Sprite* sprite_chopter;
 
 struct LevelData {
